0ea440e8725c3be24c64dbb436b892ae526c012c
loleaob <lorena.batistta@escola.pr.gov.br>
2026-03-13 10:46:43 +0000

    template

--- tenario.js
+++ tenario.js
@@ -1,1 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
 
+//Se idadeCliente for maior que idadeMinima então faça
+if (idadeCliente >= idadeMinima){
+console.log("Cerveja");
+}else {
+    console.log("Suco");
+}
+                        //condição        "true"         "false"
+console.log(idadeCliente >= idadeMinima? "cerveja" : "suco" );
